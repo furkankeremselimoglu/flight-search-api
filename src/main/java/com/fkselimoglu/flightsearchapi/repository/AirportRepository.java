3ae99412978d37909048da23240c641d5542a447
fkselimoglu <kerem.selimoglu@ozu.edu.tr>
2023-12-21 22:26:03 +0300

    Repositories are updated.

--- src/main/java/com/fkselimoglu/flightsearchapi/repository/AirportRepository.java
+++ src/main/java/com/fkselimoglu/flightsearchapi/repository/AirportRepository.java
@@ -2,7 +2,11 @@ package com.fkselimoglu.flightsearchapi.repository;
 
 import com.fkselimoglu.flightsearchapi.entity.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface AirportRepository extends JpaRepository<Airport, Long> {
-
+    List<Airport> findByCity(String city);
 }
--- src/main/java/com/fkselimoglu/flightsearchapi/repository/FlightRepository.java
+++ src/main/java/com/fkselimoglu/flightsearchapi/repository/FlightRepository.java
@@ -2,7 +2,15 @@ package com.fkselimoglu.flightsearchapi.repository;
 
 import com.fkselimoglu.flightsearchapi.entity.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+    List<Flight> findByDepartureTimeBetween(LocalDateTime start, LocalDateTime end);
+    List<Flight> findByDepartureAirportAndArrivalAirport(String departureAirport, String arrivalAirport);
+    List<Flight> findByDepartureTimeAfterAndArrivalTimeBefore(LocalDateTime departureTime, LocalDateTime arrivalTime);
 
 }
